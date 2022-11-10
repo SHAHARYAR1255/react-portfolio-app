87eb2cd1fbb6fc7e1e6fb1affe92599a20c617dd
SHAHARYAR1255 <shaharyar.malik2000@gmail.com>
2022-11-11 02:47:42 +0500

    full

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,118 @@
 import React from 'react'
 import './services.css';
+import { BiCheck } from 'react-icons/bi';
 
 function Services() {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className="container container__services">
+        <article className='services'>
+          <div className="services__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+          </ul>
+
+
+        </article>
+        {/* END OF UI/U */}
+        <article className='services'>
+          <div className="services__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+          </ul>
+
+
+        </article>
+        {/* END OF UI/U */}
+        <article className='services'>
+          <div className="services__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon'></BiCheck>
+              <p>LAF  fsaka fafk fasjfaskf</p>
+            </li>
+          </ul>
+
+
+        </article>
+        {/* END OF UI/U */}
+      </div>
+    </section>
   )
 }
 
