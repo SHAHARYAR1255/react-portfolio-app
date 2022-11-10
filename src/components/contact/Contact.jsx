30b290a9093a7c14bb1265366ab6b0dec9dfd4cc
SHAHARYAR1255 <shaharyar.malik2000@gmail.com>
2022-11-11 04:00:13 +0500

    full2

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+import emailjs from 'emailjs-com';
+
 
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form.current, process.env.YOUR_PUBLIC_KEY)
+      .then((result) => {
+        console.log(result.text);
+      }).catch((error) => {
+        console.log(error.text);
+      })
+  };
+
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon' ></MdOutlineEmail>
+            <h4>Email</h4>
+            <h5>shaharyar.malik2000</h5>
+            <a href="mailto:shaharyar.malik2000@gmail.com" rel="noreferrer" target="_blank">Send an Email</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Shaharyar Malik</h5>
+            <a href="https://m.me/shaharyarm1" target="_blank" rel="noreferrer" >Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>Whatsapp</h4>
+            <h5>+923172039912</h5>
+            <a href="https://api.whatsapp.com/send?phone=+923172039912" rel="noreferrer" target="_blank">Send a Message</a>
+          </article>
+        </div>
+        <form action={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Full Name' required />
+          <input type="email" name='emaiil' placeholder='Your Email' required />
+          <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
+          <button type="submit" className='btn btn-primary'>Send a Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,100 @@
 import React from 'react'
 import './testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+import AVTR4 from '../../assets/avatar4.jpg';
+
+// core version + navigation, pagination modules:
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+// init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+//   modules: [Navigation, Pagination],
+
+// });
 
 function Testimonials() {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+
+        className="container testimonials__container">
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="" />
+          </div>
+          <h5 className='client__name'>namfmdwmdwd sadadd</h5>
+          <small className='client__review'>
+
+            the leader in cloud-based software for the global life sciences industry. Committed to innovation, product excellence,
+            and customer success, our customers range from the world’s largest pharmaceutical companies to emerging biotechs. Veeva’s
+            software helps our customers bring medicines and therapies to patients faster.
+
+
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="" />
+          </div>
+          <h5 className='client__name'>namfmdwmdwd sadadd</h5>
+          <small className='client__review'>
+
+            the leader in cloud-based software for the global life sciences industry. Committed to innovation, product excellence,
+            and customer success, our customers range from the world’s largest pharmaceutical companies to emerging biotechs. Veeva’s
+            software helps our customers bring medicines and therapies to patients faster.
+
+
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="" />
+          </div>
+          <h5 className='client__name'>namfmdwmdwd sadadd</h5>
+          <small className='client__review'>
+
+            the leader in cloud-based software for the global life sciences industry. Committed to innovation, product excellence,
+            and customer success, our customers range from the world’s largest pharmaceutical companies to emerging biotechs. Veeva’s
+            software helps our customers bring medicines and therapies to patients faster.
+
+
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="" />
+          </div>
+          <h5 className='client__name'>namfmdwmdwd sadadd</h5>
+          <small className='client__review'>
+
+            the leader in cloud-based software for the global life sciences industry. Committed to innovation, product excellence,
+            and customer success, our customers range from the world’s largest pharmaceutical companies to emerging biotechs. Veeva’s
+            software helps our customers bring medicines and therapies to patients faster.
+
+
+          </small>
+        </SwiperSlide>
+
+      </Swiper >
+    </section >
   )
 }
 
