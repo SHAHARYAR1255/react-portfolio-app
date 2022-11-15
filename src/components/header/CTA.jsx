6fd5dfcfd8f9e035ff19f1c20d4ab9285fdad2c3
SHAHARYAR1255 <shaharyar.malik2000@gmail.com>
2022-11-16 02:10:57 +0500

    cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,12 +3,31 @@ import CV from '../../assets/cv.pdf';
 
 
 function CTA() {
+  // Function will execute on click of button
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch(CV).then(response => {
+      response.blob().then(blob => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = CV;
+        alink.click();
+      })
+    })
+  }
+
   return (
     <div className="cta">
-        <a href={CV} className="btn">Download CV</a>
-        <a href="#contact" className='btn'> Lets talk</a>
+      {/* <a href={CV} className="btn">Download CV</a> */}
+      <a className="btn" onClick={onButtonClick}>
+        Download CV
+      </a>
+      <a href="#contact" className='btn'> Lets talk</a>
     </div>
-    )
+  )
 }
 
 export default CTA
